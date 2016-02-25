aec77c8371375a9aacb3d124ae4d9784756d556b
Matthew Polizzotti <mpolizzotti@unicon.net>
2016-02-24 21:48:31 -0700

    Add more tests to arrow.spec.js

--- client/js/src/arrow/arrow.spec.js
+++ client/js/src/arrow/arrow.spec.js
@@ -10,8 +10,10 @@ describe('arrow functions', function () {
 
     it('can provide a compact syntax without function braces', function () {
         let add = (x, y) => x + y;
+        let square = x => x * x;
 
         expect(add(2, 3)).toBe(5);
+        expect(square(2)).toBe(4);
     });
 
     it('can provide a compact syntax without parameter parenthesis or function braces', function () {
@@ -20,32 +22,23 @@ describe('arrow functions', function () {
         expect(double(10)).toBe(100);
     });
 
-    it('can allow the use of the lexical this keyword', function () {
-        let FooService = {
-            doSomething: function (callback) {
-                return callback('World');
-            },
-            doSomethingElse: function (callback) {
-                return callback('World');
-            }
-        };
+    it('can be used with array methods', function() {
+        let numbers = [1, 2, 3, 4];
 
-        let FooCtrl = function (FooService) {
-            var that = this;
-            that.foo = 'Hello';
-            FooService.doSomethingElse(function (response) {
-                that.foo = that.foo + response;
-                expect(that.foo).toBe('HelloWorld');
-            });
-            
-            
-            this.foo = 'Hello';
-            FooService.doSomething((response) => {
-                this.foo = this.foo + response;
-                expect(this.foo).toBe('HelloWorld');
-            });
-        }
+        let sum = 0;
+        numbers.forEach(n => sum += n);
+        expect(sum).toBe(10);
 
-        let foo = new FooCtrl(FooService);
+        let doubled = numbers.map(n => n * 2);
+        expect(doubled).toEqual([2, 4, 6, 8]);
+    });
+
+    it('can allow the use of the lexical this keyword', function (done) {
+        this.name = 'Matthew';
+
+        let t = setTimeout(() => {
+            expect(this.name).toBe('Matthew');
+            done();
+        }, 100);
     });
 });
