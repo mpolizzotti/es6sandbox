05ac0daaa17385b06991c1ff4c2a86c822ef1707
Matthew Polizzotti <mpolizzotti@unicon.net>
2016-02-29 16:51:21 -0700

    Add additional iterator test example.

--- client/js/src/iterators/iterators.spec.js
+++ client/js/src/iterators/iterators.spec.js
@@ -40,6 +40,49 @@ describe('iterators', function () {
 
         expect(sum).toBe(10);
     });
+
+    it('can be used to implement a custom iterator.', function () {
+        class ArrayIterator {
+            constructor(array) {
+                this.array = array;
+                this.index = 0;
+            }
+
+            next() {
+                var result = {value: undefined, done: true};
+                if (this.index < this.array.length) {
+                    result.value = this.array[this.index];
+                    result.done = false;
+                    this.index = this.index + 1;
+                }
+                return result;
+            }
+        }
+
+        class Company {
+            constructor() {
+                this.employees = [];
+            }
+
+            addEmployees(...names) {
+                this.employees = this.employees.concat(names);
+            }
+
+            [Symbol.iterator]() {
+                return new ArrayIterator(this.employees);
+            }
+        }
+
+        let count = 0;
+        let company = new Company();
+        company.addEmployees('Tim', 'Sue', 'Joy', 'Tom');
+
+        for (let employee of company) {
+            count = count + 1;
+        }
+
+        expect(count).toBe(4);
+    });
 });
 
 describe('for of loop', function () {
@@ -54,9 +97,3 @@ describe('for of loop', function () {
         expect(sum).toBe(10);
     });
 });
-
-
-
-
-
-
